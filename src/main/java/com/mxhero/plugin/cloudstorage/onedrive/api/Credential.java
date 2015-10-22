a02c090894fbdf9c3ccd53894629505f1ddef765
Marcelo Marmol <mmarmol@mxhero.com>
2015-10-22 11:12:25 -0200

    added license information

--- src/main/java/com/mxhero/plugin/cloudstorage/onedrive/api/Credential.java
+++ src/main/java/com/mxhero/plugin/cloudstorage/onedrive/api/Credential.java
@@ -1,23 +1,17 @@
-/*
- * mxHero is a platform that intends to provide a single point of development 
- * and single point of distribution for email solutions and enhancements. It does this
- * by providing an extensible framework for rapid development and deployment of
- * email solutions.
- * 
- * Copyright (C) 2012  mxHero Inc.
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Copyright (C) 2015 mxHero Inc (mxhero@mxhero.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.mxhero.plugin.cloudstorage.onedrive.api;
 
