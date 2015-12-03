f127af353dcc1ce9354f53d214574e8a0cba75c7
jproyo <jproyo@mxhero.com>
2015-12-03 10:38:09 +0100

    #ENGINE-318: Implemented

--- src/test/java/com/mxhero/plugin/cloudstorage/onedrive/api/BusinessCredentialTest.java
+++ src/test/java/com/mxhero/plugin/cloudstorage/onedrive/api/BusinessCredentialTest.java
@@ -1,4 +1,5 @@
-/* Copyright (C) 2015 mxHero Inc (mxhero@mxhero.com)
+/**
+ * Copyright (C) 2015 mxHero Inc (mxhero@mxhero.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
