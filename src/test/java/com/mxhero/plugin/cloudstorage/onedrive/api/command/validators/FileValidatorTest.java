dfe0c0b74631892430e60864e10a4db2206648b2
Juan Pablo Royo <juanpablo.royo@gmail.com>
2015-12-10 21:08:25 +0100

    #ENGINE-319: Implement Business API Restrictions

--- src/test/java/com/mxhero/plugin/cloudstorage/onedrive/api/command/validators/FileValidatorTest.java
+++ src/test/java/com/mxhero/plugin/cloudstorage/onedrive/api/command/validators/FileValidatorTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2015 mxHero Inc (mxhero@mxhero.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.mxhero.plugin.cloudstorage.onedrive.api.command.validators;
 
 import static org.junit.Assert.fail;
@@ -8,8 +23,14 @@ import java.util.UUID;
 
 import org.junit.Test;
 
+/**
+ * The Class FileValidatorTest.
+ */
 public class FileValidatorTest {
 
+	/**
+	 * Test validate not throw exception.
+	 */
 	@Test
 	public void testValidateNotThrowException() {
 		try {
@@ -19,6 +40,9 @@ public class FileValidatorTest {
 		}
 	}
 
+	/**
+	 * Test validate fail because empty file.
+	 */
 	@Test
 	public void testValidateFailBecauseEmptyFile() {
 		FileValidator validator = new FileValidator();
@@ -34,6 +58,9 @@ public class FileValidatorTest {
 		}
 	}
 	
+	/**
+	 * Test validate fail because wrong extension.
+	 */
 	@Test
 	public void testValidateFailBecauseWrongExtension() {
 		FileValidator validator = new FileValidator();
@@ -47,6 +74,9 @@ public class FileValidatorTest {
 		}
 	}
 	
+	/**
+	 * Test validate fail because wrong name.
+	 */
 	@Test
 	public void testValidateFailBecauseWrongName() {
 		FileValidator validator = new FileValidator();
@@ -60,6 +90,9 @@ public class FileValidatorTest {
 		}
 	}
 	
+	/**
+	 * Test validate fail because wrong guid.
+	 */
 	@Test
 	public void testValidateFailBecauseWrongGuid() {
 		FileValidator validator = new FileValidator();
--- src/test/java/com/mxhero/plugin/cloudstorage/onedrive/api/command/validators/FolderValidatorTest.java
+++ src/test/java/com/mxhero/plugin/cloudstorage/onedrive/api/command/validators/FolderValidatorTest.java
@@ -1,11 +1,32 @@
+/**
+ * Copyright (C) 2015 mxHero Inc (mxhero@mxhero.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.mxhero.plugin.cloudstorage.onedrive.api.command.validators;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * The Class FolderValidatorTest.
+ */
 public class FolderValidatorTest {
 	
+	/**
+	 * Test validate not throw exception.
+	 */
 	@Test
 	public void testValidateNotThrowException() {
 		try {
@@ -15,6 +36,9 @@ public class FolderValidatorTest {
 		}
 	}
 
+	/**
+	 * Test validate fail empty folder.
+	 */
 	@Test
 	public void testValidateFailEmptyFolder() {
 		try {
@@ -29,6 +53,9 @@ public class FolderValidatorTest {
 		}
 	}
 	
+	/**
+	 * Test validate fail forbidden names.
+	 */
 	@Test
 	public void testValidateFailForbiddenNames() {
 		String[] split = "files|file|Dateien|fichiers|bestanden||archivos|tiedostot|pliki|soubory|elemei|ficheiros|arquivos|dosyalar|datoteke|fitxers|failid|fails|bylos|fajlovi|fitxategiak|private".split("\\|");
